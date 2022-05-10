78ce747e502cc7c93d103a0e8bad1ab0c758cbeb
Nikita Stroganov <findoss@yandex.ru>
2022-05-10 14:36:45 +0300

    hotfix

--- sites/kuaikanmanhua-com.js
+++ sites/kuaikanmanhua-com.js
@@ -36,7 +36,7 @@ export const getManga = async () => {
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       },
     })
-    .then(() => {
+    .then((response) => {
       if (response.status === 200) {
         return response.data;
       } else {
