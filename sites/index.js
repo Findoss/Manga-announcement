eb5bc58cf6a91900f64d3f7f172d95874b0ee682
Nikita Stroganov <findoss@yandex.ru>
2022-05-14 11:26:12 +0300

    uocommit sites

--- sites/index.js
+++ sites/index.js
@@ -3,7 +3,7 @@ import { manga_kuaikanmanhua } from "./kuaikanmanhua-com.js";
 import { manga_ac_qq_com } from "./ac-qq-com.js";
 
 export const sites = {
-  // manga_bilibili_com: manga_bilibili_com,
-  // manga_kuaikanmanhua: manga_kuaikanmanhua,
-  // manga_ac_qq_com: manga_ac_qq_com,
+  manga_bilibili_com: manga_bilibili_com,
+  manga_kuaikanmanhua: manga_kuaikanmanhua,
+  manga_ac_qq_com: manga_ac_qq_com,
 };
